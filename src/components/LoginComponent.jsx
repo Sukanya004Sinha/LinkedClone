2d7db7bbb166a9c0bb67c440a77d477ebedd499d
sukanya004sinha <sukanyasinha004@gmail.com>
2023-09-09 18:10:22 +0530

    initial commit

---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LoginAPI } from '../api/AuthAPI';
+
+export default function LoginComponent() {
+  return (
+    <div><h1>LoginComponent</h1>
+    <button className='login-btn'>Log IN to Linkedin</button>
+    </div>
+  )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGRcxNdmWbwWjS81tew9KhhcgcnIU1ScE",
+  authDomain: "linkedin-clone-7fcb5.firebaseapp.com",
+  projectId: "linkedin-clone-7fcb5",
+  storageBucket: "linkedin-clone-7fcb5.appspot.com",
+  messagingSenderId: "757019879781",
+  appId: "1:757019879781:web:4b425147cea9e0ee44198c",
+  measurementId: "G-9CXYL1GHWR"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
